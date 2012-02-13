ca4484d83e3fc872c7eb3a8cad61b0b786cbefc4
Paul Hoehne <phoehne@mac.com>
2012-02-12 23:27:49 -0500

    Writing tests to handle basic request/response inproc addresses are not working but the test works with tcp and ipc addresses.  testTCPReqResp and testIPCReqResp test simple request repines sockets.

--- ZeroMQKitTests/TestBasicFunctions.m
+++ ZeroMQKitTests/TestBasicFunctions.m
@@ -10,9 +10,14 @@
 #import "TestBasicFunctions.h"
 
 @implementation TestBasicFunctions
+NSString* INPROC_ADDRESS = @"inproc://testreqrep";
+NSString* IPC_ADDRESS = @"ipc:///tmp/testreqrep";
+NSString* TCP_ADDRESS = @"tcp://0.0.0.0:5400";
+
+static void* ctx; 
 
 // All code under test must be linked into the Unit Test bundle
-- (void)testBackAndForth
+- (void)testVersionNumbers
 {
     int major, minor, patch;
     
@@ -26,27 +31,60 @@
     zmq_term(ctx);
 }
 
--(void) testSimpleRequestResponse 
-{
-    void* ctx = zmq_init(1);
-    void* sock = zmq_socket(ctx, ZMQ_REQ);
-    zmq_connect(sock, "tcp://0.0.0.0:5401");
-    zmq_msg_t request;
-    zmq_msg_t response;
+-(void) requestResponseServer: (void*)val {
+    void* sock = zmq_socket(ctx, ZMQ_REP);
     
-    zmq_msg_init_size(&request, 10);
-    memcpy(zmq_msg_data(&request), "1234567890", 10);
+    zmq_bind(sock, [(__bridge NSString*)val cStringUsingEncoding:NSUTF8StringEncoding]);
+    NSLog(@"Binding socket");
+    zmq_msg_t request, response;
+    
+    zmq_msg_init(&request);
+    NSLog(@"Initializing message");
+    
+    zmq_recvmsg(sock, &request, 0);
+    NSLog(@"Received message");
+    
+    zmq_msg_init_size(&response, zmq_msg_size(&request));
+    memcpy(zmq_msg_data(&response), zmq_msg_data(&request), zmq_msg_size(&request));
+    zmq_sendmsg(sock, &response, 0);
+    NSLog(@"Sent response");
+    
+    zmq_msg_close(&request);
+    zmq_msg_close(&response);
+    zmq_close(sock);
+}
+
+-(void) runReqRespTest:(NSString*)binding {
+    ctx = zmq_init(2);
+    void* sock = zmq_socket(ctx, ZMQ_REQ);
+    
+    [NSThread detachNewThreadSelector:@selector(requestResponseServer:) toTarget:self withObject:binding];
+    
+    
+    zmq_connect(sock, [binding cStringUsingEncoding:NSUTF8StringEncoding]);
+    zmq_msg_t request, response;
+    
+    zmq_msg_init_size(&request, 5);
+    memcpy(zmq_msg_data(&request), "12345", 5);
     zmq_sendmsg(sock, &request, 0);
     
     zmq_msg_init(&response);
     zmq_recvmsg(sock, &response, 0);
-
-    STAssertEquals(zmq_msg_size(&response), (size_t)5, @"Should have 5 bytes");
-    STAssertTrue(strncmp("12345", zmq_msg_data(&response), 5) == 0, @"Shoudl be equal to 12345");
+    STAssertTrue(memcmp(zmq_msg_data(&response), "12345", 5) == 0, @"Should be equal to 12345");
     
+    zmq_msg_close(&request);
     zmq_msg_close(&response);
     zmq_close(sock);
     zmq_term(ctx);
+
+}
+
+-(void) testTCPReqResp {
+    [self runReqRespTest:TCP_ADDRESS];
+}
+
+-(void) testIPCReqResp {
+    [self runReqRespTest:IPC_ADDRESS];
 }
 
 @end
